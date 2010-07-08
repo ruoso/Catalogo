d0391257a22f7aedecf36b281acee7becbd6a59f
Daniel Ruoso <daniel@ruoso.com>
2010-07-07 23:05:01 -0300

    breadcrumb de sessao implementado... mais util nessa aplicacao que o breadcrumb de path

--- lib/Catalogo.pm
+++ lib/Catalogo.pm
@@ -1,16 +1,7 @@
 package Catalogo;
 use Moose;
 use namespace::autoclean;
-
 use Catalyst::Runtime 5.80;
-
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
 
 use Catalyst qw/
     -Debug
@@ -23,84 +14,61 @@ use Catalyst qw/
     Static::Simple
 /;
 
-__PACKAGE__->config( 'Plugin::Authentication' =>
-                    {
-                        default_realm => 'members',
-                        realms => {
-                            members => {
-                                credential => {
-                                    class => 'Password',
-                                    password_field => 'password',
-                                    password_type => 'clear'
-                                },
-                                store => {
-                                    class => 'Minimal',
-                                    users => {
-                                        bob => {
-                                            password => "123",
-                                            editor => 'yes',
-                                            roles => [qw/edit delete/],
-                                        },
-                                        william => {
-                                            password => "456",
-                                            roles => [qw/comment/],
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-    );
+
+# autenticacao para desenvolvimento
+__PACKAGE__->config(     );
 
 extends 'Catalyst';
 
 our $VERSION = '0.01';
 $VERSION = eval $VERSION;
 
-# Configure the application.
-#
-# Note that settings in catalogo.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
-
-__PACKAGE__->config(
-    name => 'Catalogo',
+__PACKAGE__->config
+  ( name => 'Catalogo',
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
-);
+    # test auth
+    'Plugin::Authentication' =>
+    { default_realm => 'members',
+      realms =>
+      { members =>
+        { credential =>
+          { class => 'Password',
+            password_field => 'password',
+            password_type => 'clear' },
+          store =>
+          { class => 'Minimal',
+            users =>
+            { bob =>
+              { password => "123",
+                editor => 'yes',
+                roles => [qw/edit delete/] },
+              william =>
+              { password => "456",
+                roles => [qw/comment/] } } } } } }
+  );
 
 # Start the application
 __PACKAGE__->setup();
 
+sub place_bread {
+    my ($self, $uri, $label) = @_;
 
-=head1 NAME
+    my $bc = $self->session->{breadcrumb} || [];
+    $bc = [ grep { $_->{uri} &&
+                     $_->{uri} ne $uri } @$bc ];
 
-Catalogo - Catalyst based application
+    push @$bc, { uri => $uri, label => $label };
+    splice @$bc, 0, -5;
 
-=head1 SYNOPSIS
+    $self->session->{breadcrumb} = $bc;
 
-    script/catalogo_server.pl
+    return undef;
+};
 
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 SEE ALSO
-
-L<Catalogo::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-Henrique Mota,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
+sub breadcrumb {
+    my ($self) = @_;
+    return $self->session->{breadcrumb};
+}
 
 1;
